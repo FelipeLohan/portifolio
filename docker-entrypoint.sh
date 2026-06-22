a6ff484686abb7e0e29400cde8d928c239d24663
FelipeLohan <felipelohan03052006@gmail.com>
2026-06-22 09:43:26 -0300

    refactor: removed nginx

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Start Angular SSR server in background
-node /app/dist/portifolio/server/server.mjs &
-
-# Start nginx in foreground (keeps container alive)
-exec nginx -g 'daemon off;'
